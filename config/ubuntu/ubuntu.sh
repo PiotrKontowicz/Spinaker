83728205405d2bbd63661c2ff0a8e06c2a31efdd
Piotr Kontowicz <piotr.kontowicz@put.poznan.pl>
2023-01-30 20:31:43 +0100

    Update ubuntu.sh

--- config/ubuntu/ubuntu.sh
+++ config/ubuntu/ubuntu.sh
@@ -1,1 +1,6 @@
 sudo apt update
+uname="student"
+upass="student"
+sudo adduser --quiet --disabled-password --shel /bin/bash --home /home/$uname --gecos "" $uname
+sudo usermod -a -G sudo $uname
+sudo reboot
